f06e7ab5e387899592fb7fcbf72b8e3dc02a7b2e
Awilliams4321 <awilliams1234@live.com>
2020-12-14 22:14:11 -0700

    Adding Bill properties to the constructor function.

--- src/bill.js
+++ src/bill.js
@@ -1,3 +1,12 @@
 class Bill {
+    constructor(name, creditor, balance_owed, monthly_payment, due_date, user_id) {
+        this.name = name,
+        this.creditor = creditor,
+        this.balance_owed = balance_owed,
+        this.monthly_payment = monthly_payment,
+        this.due_date = due_date,
+        this.user_id = user_id
+    }
+
     
 }
