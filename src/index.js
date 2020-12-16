5738739af10662dd2605ff75523e90b1d262cc7f
Awilliams4321 <awilliams1234@live.com>
2020-12-16 12:28:55 -0700

    Adding an event listener to my user form.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 const billForm = document.getElementById("bill-form")
+const userForm = document.getElementById("user-form")
 
 billForm.addEventListener("submit", submitBillForm)
+userForm.addEventListener("submit", submitUserForm)
 
 function submitBillForm(e) {
     e.preventDefault()
     console.log("submitted")
+}
+
+function submitUserForm(e) {
+    e.preventDefault()
+    console.log("user created")
 }
