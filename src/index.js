c3c009c3f1f793d1d84d6b3b3bf8b64d6d81cb57
Awilliams4321 <awilliams1234@live.com>
2020-12-17 11:04:47 -0700

    Deleting all user related functions and variables because the user model is no longer being used.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 const billForm = document.getElementById("bill-form")
-const userForm = document.getElementById("user-form")
 
 billForm.addEventListener("submit", submitBillForm)
-userForm.addEventListener("submit", submitUserForm)
 
 function submitBillForm(e) {
     e.preventDefault()
     console.log("submitted")
-}
-
-function submitUserForm(e) {
-    e.preventDefault()
-    console.log("user created")
 }
