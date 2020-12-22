ac327c44a3d71105316375209086ffbf5c33a0d4
Awilliams4321 <awilliams1234@live.com>
2020-12-22 10:08:01 -0700

    Adding a GET fetch 'class' method to grab all categories from the database and display them on the DOM with my render categories method.

--- src/category.js
+++ src/category.js
@@ -1,5 +1,35 @@
 class Category {
-    constructor(name) {
-        this.name = name,
+
+    static allCategories = []
+
+    constructor(category) {
+        this.id = category.attributes.id
+        this.name = category.attributes.name
+        Category.allCategories.push(this)
     }
+
+    static renderAllCategories() {
+        for(let category of this.allCategories){
+            category.renderCategories()
+        }
+    }
+
+    static fetchCategories() {
+        fetch(categoriesURL)
+        .then(response => response.json())
+        .then(categories => {
+            categories.data.forEach(console.log)
+        })
+    }
+
+    static renderCategories() {
+        const li = document.createElement("li")
+        li.dataset.id = this.id
+
+        const p = document.createElement("p")
+        p.innerText = this.name
+
+        categoryList.appendChild(p)
+    }
+
 }
