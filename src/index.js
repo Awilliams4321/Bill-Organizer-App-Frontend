c6c926ee02aa792dfa44ded107f6b69c02afe741
Awilliams4321 <awilliams1234@live.com>
2020-12-24 10:00:10 -0700

    Creating variables and calling Categories.fetchCategories().

--- src/index.js
+++ src/index.js
@@ -1,8 +1,45 @@
+const billsUrl = "http://localhost:3000/bills"
+const categoriesURL = "http://localhost:3000/categories"
+
+const categoryList = document.getElementById("category-names")
+
 const billForm = document.getElementById("bill-form")
+const billName = document.getElementById("bill-name")
+const creditor = document.getElementById("creditor")
+const balanceOwed = document.getElementById("balance-owed")
+const monthlyPayment = document.getElementById("monthly-payment")
+const dueDate = document.getElementById("due-date")
+
+Category.fetchCategories()
+
+// fetchBills()
+
+
 
 billForm.addEventListener("submit", submitBillForm)
 
 function submitBillForm(e) {
     e.preventDefault()
-    console.log("submitted")
+    // console.log() 
 }
+
+// function fetchBills() {
+//     fetch(billsUrl)
+//     .then(response => response.json())
+//     .then(bills => {
+//         for(let bill of bills){
+//             let newBill = new Bill(bill.data)
+//         }
+//         this.renderBills()
+//     }
+// }
+
+function renderBills(bills) {
+    const ul = getElementById("category-names")
+    bills.forEach(bill => {
+        const h3 = document.createElement("h3")
+        h3.innerHTML = bill.name
+        ul.appendChild(h3)
+    })
+}
+
