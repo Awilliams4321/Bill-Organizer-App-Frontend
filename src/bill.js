010966c5575bbd84678dd304d611ddf0913a7f07
Awilliams4321 <awilliams1234@live.com>
2020-12-28 21:05:38 -0700

    Creating a billForm static method that will handle my form submission. I am grabbing the values of the forms input ids and setting them equal to variables for later use.

--- src/bill.js
+++ src/bill.js
@@ -1,12 +1,26 @@
 class Bill {
-    constructor(name, creditor, balance_owed, monthly_payment, due_date, user_id) {
-        this.name = name,
-        this.creditor = creditor,
-        this.balance_owed = balance_owed,
-        this.monthly_payment = monthly_payment,
-        this.due_date = due_date,
-        this.category_id = category_id
+
+    static allBills = []
+
+    constructor(bill) {
+        this.id = bill.attributes.id
+        this.name = bill.attributes.name
+        this.creditor = bill.attributes.creditor
+        this.balance_owed = bill.attributes.balance_owed
+        this.monthly_payment = bill.attributes.monthly_payment
+        this.due_date = bill.attributes.due_date
+        this.category_id = bill.attributes.category_id
+        Bills.allBills.push(this)
     }
 
+    static billForm(e) {
+        const billName = document.getElementById("bill-name").value
+        const creditor = document.getElementById("creditor").value
+        const balanceOwed = document.getElementById("balance-owed").value
+        const monthlyPayment = document.getElementById("monthly-payment").value
+        const dueDate = document.getElementById("due-date").value
+        
+        debugger
+    }
     
 }
