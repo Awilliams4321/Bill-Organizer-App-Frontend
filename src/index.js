2fb8b45dab748c76d14da82a9eacc15f6741df39
Awilliams4321 <awilliams1234@live.com>
2021-01-11 08:08:18 -0700

    Sending my submit event to my Bill.postBill callback function. Calling Bill.fetchBills() so that all existing bills are immediately rendered upon DOM load.

--- src/index.js
+++ src/index.js
@@ -4,17 +4,7 @@ const categoriesURL = "http://localhost:3000/categories"
 const categoryList = document.getElementById("category-names")
 const submitBillForm = document.getElementById("bill-form")
 
+submitBillForm.addEventListener("submit", Bill.postBill)
 
-Category.fetchCategories()
-submitBillForm.addEventListener("submit", Bill.billForm)
+Bill.fetchBills()
 
-
-
-// function renderBills(bills) {
-//     const ul = getElementById("category-names")
-//     bills.forEach(bill => {
-//         const h3 = document.createElement("h3")
-//         h3.innerHTML = bill.name
-//         ul.appendChild(h3)
-//     })
-// }
