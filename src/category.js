56b9936e3c7dc7ddccda81892688f865ff76f026
Awilliams4321 <awilliams1234@live.com>
2021-01-11 08:06:06 -0700

    Because a bill belongs to a category and i can render it using my category attribute in my Bill serializer, the Category class is unnecessary and has been commented out for the time being.

--- src/category.js
+++ src/category.js
@@ -1,46 +1,46 @@
-class Category {
+// class Category {
 
-    static allCategories = []
+//     static allCategories = []
 
-    constructor(category) {
-        this.id = category.id
-        this.name = category.attributes.name
-        this.bills = category.attributes.bills
-        Category.allCategories.push(this)
-    }
+//     constructor(category) {
+//         this.id = category.id
+//         this.name = category.attributes.name
+//         this.bills = category.attributes.bills
+//         Category.allCategories.push(this)
+//     }
 
-    // static renderAllCategories() {
-    //     for(let category of this.allCategories){
-    //         category.renderCategories()
-    //     }
-    // }
+//     // static renderAllCategories() {
+//     //     for(let category of this.allCategories){
+//     //         category.renderCategories()
+//     //     }
+//     // }
 
-    static fetchCategories() {
-        fetch(categoriesURL)
-        .then(response => response.json())
-        .then(categories => {
-            categories.data.forEach(category => {
-                let newCategory = new Category(category)
-                // console.log(newCategory)
-                newCategory.renderCategories()
-            })
-        })
-    }
+//     static fetchCategories() {
+//         fetch(categoriesURL)
+//         .then(response => response.json())
+//         .then(categories => {
+//             categories.data.forEach(category => {
+//                 let newCategory = new Category(category)
+//                 // console.log(newCategory)
+//                 newCategory.renderCategories()
+//             })
+//         })
+//     }
 
-    renderCategories() {
-        const li = document.createElement("li")
-        li.dataset.id = this.id
+//     renderCategories() {
+//         const li = document.createElement("li")
+//         li.dataset.id = this.id
 
-        const p = document.createElement("p")
-        p.innerText = this.name
+//         const p = document.createElement("p")
+//         p.innerText = this.name
 
-        this.bills.forEach(bill => {
-            let billObj = new Bill(bill)
-            console.log(billObj)
-            billObj.renderBills()
-        })
+//         this.bills.forEach(bill => {
+//             let billObj = new Bill(bill)
+//             // console.log(billObj)
+//             billObj.renderBills()
+//         })
 
-        li.appendChild(p)
-        categoryList.appendChild(li)
-    }
-}
+//         li.appendChild(p)
+//         categoryList.appendChild(li)
+//     }
+// }
