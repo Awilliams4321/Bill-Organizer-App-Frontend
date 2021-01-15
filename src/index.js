4825b88b2f125a98a60ec8ea9c0bc7d32cdfc666
Awilliams4321 <awilliams1234@live.com>
2021-01-14 21:45:18 -0700

    Deleting commented out code.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ const categoriesURL = "http://localhost:3000/categories"
 
 const categoryList = document.getElementById("category-names")
 const submitBillForm = document.getElementById("bill-form")
+
 const assessTest = document.getElementById("test")
-
-
-submitBillForm.addEventListener("submit", Bill.postBill)
-
 assessTest.addEventListener("change", Bill.renderCategory)
 
+submitBillForm.addEventListener("submit", Bill.postBill)
 
 Bill.fetchBills()
 
