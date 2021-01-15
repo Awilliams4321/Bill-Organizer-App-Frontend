f8c825c2f918e7c0db111f421f550868f81db6ab
Awilliams4321 <awilliams1234@live.com>
2021-01-14 20:55:45 -0700

    Assessment code challenge, adding a dynamic dropdown menu that diplays ONLY one categories bills.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ const categoriesURL = "http://localhost:3000/categories"
 
 const categoryList = document.getElementById("category-names")
 const submitBillForm = document.getElementById("bill-form")
+const assessTest = document.getElementById("test")
+
 
 submitBillForm.addEventListener("submit", Bill.postBill)
+
+assessTest.addEventListener("change", Bill.renderCategory)
+
 
 Bill.fetchBills()
 
